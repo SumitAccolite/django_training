11e047e57aefe4dab80884cf3063a4b732c71019
SumitAccolite <sumit.bashetwar@accolitedigital.com>
2024-02-10 13:00:39 +0530

    Django first app

--- practicetutorial/home/models.py
+++ practicetutorial/home/models.py
@@ -8,3 +8,12 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
